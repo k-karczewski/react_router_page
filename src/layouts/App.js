0964b28a8196ec4a93bc45023498d109fcb60966
Konrad Karczewski <kkarczewski94@gmail.com>
2020-10-09 12:04:53 +0200

    Component structure and styles

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../styles/App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Header from './Header';
+import Page from './Page';
+import Navigation from './Navigation';
+import Footer from './Footer';
 
-function App() {
-  return (
-    <div className="App">
-      HELLO WORLD
-    </div>
-  );
+class App extends Component {
+  render(){ 
+    return (
+      <Router>
+        <div className="app">
+          <header>
+            {<Header />}
+          </header>
+          <main>
+            <aside>
+              {<Navigation />}
+            </aside>
+            <section className="page">
+              {<Page />}
+            </section>
+          </main>
+          <footer>
+            {<Footer /> }
+          </footer>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
