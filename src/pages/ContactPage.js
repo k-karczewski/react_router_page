27f5402d009baea3dc7382353ece29e9d6210f15
Konrad Karczewski <kkarczewski94@gmail.com>
2020-10-09 17:57:26 +0200

    Contact page with Prompt component

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import "../styles/ContactPage.css";
+import {Prompt} from "react-router-dom";
 
-const ContactPage = () => {
-    return ( 
-        <div>strona kontaktu</div>
-     );
+class ContactPage extends Component {
+    state = {
+        value: ""
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({
+            value: "",
+        })
+    }
+
+    handleChange = event => {
+        this.setState({
+            value: event.target.value,
+        });
+    }
+
+    render() { 
+        return ( 
+            <div className="contact">
+                <form onSubmit={this.handleSubmit}>
+                    <h3>Napisz do nas</h3>
+                    <textarea 
+                        value={this.state.value} 
+                        onChange={this.handleChange}
+                        placeholder="Wpisz wiadomość..."/>
+                    <button>Wyślij</button>
+                </form>
+                <Prompt
+                    when={this.state.value}
+                    message="Masz niezapisane zmiany. 
+                            Czy na pewno chcesz opuścić tę stronę?" 
+                    />
+            </div>
+        );
+    }
 }
  
 export default ContactPage;
