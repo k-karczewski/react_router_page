3cdd195323b5e1f9cfa2f008e8388376c47b66cc
Konrad Karczewski <kkarczewski94@gmail.com>
2020-10-10 18:50:32 +0200

    Updates

---
+++ precache-manifest.9087b358ca181b58e6f4589e41502486.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96ea1b99b25291c37922937fc8b58c56",
+    "url": "/react_router_page/index.html"
+  },
+  {
+    "revision": "4650b838d4f4836194ea",
+    "url": "/react_router_page/static/css/main.e6716c47.chunk.css"
+  },
+  {
+    "revision": "a9c723bd2570b78c9a3e",
+    "url": "/react_router_page/static/js/2.045ca824.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_router_page/static/js/2.045ca824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4650b838d4f4836194ea",
+    "url": "/react_router_page/static/js/main.982ee272.chunk.js"
+  },
+  {
+    "revision": "454583d80dd50c23e5cb",
+    "url": "/react_router_page/static/js/runtime-main.443dfe18.js"
+  },
+  {
+    "revision": "33a5b37483eaa2a33408f7ae5c0c3f04",
+    "url": "/react_router_page/static/media/header1.33a5b374.jpg"
+  },
+  {
+    "revision": "448805cbf935cdb9c8fc9bfe7afce849",
+    "url": "/react_router_page/static/media/header2.448805cb.jpg"
+  },
+  {
+    "revision": "b31c04b8ccff29064f5e9d805878b262",
+    "url": "/react_router_page/static/media/header3.b31c04b8.jpg"
+  }
+]);
